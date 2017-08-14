fa729bf5c6ccf80c752f1be1a31cb410a4164927
darkshinobee <ahmedibrahim3000@gmail.com>
2017-08-14 10:47:51 +0100

    rebranching

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'test@event_man.com'),
+        'name' => env('MAIL_FROM_NAME', 'Event_Man'),
     ],
 
     /*
