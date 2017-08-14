4f67e975dd64af7f0d8e54e1d534f11dba7fcf44
darkshinobee <ahmedibrahim3000@gmail.com>
2017-08-14 17:31:06 +0100

    homepage themefied

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,17 @@
 @extends('main')
 @section('title', 'Homepage')
 @section('content')
-
+  <section class="hero-1">
+    <div class="container">
+      <div class="row">
+        <div class="hero-content">
+          <h1 class="hero-title">Make Your Dream Come True</h1>
+          <p class="hero-caption">Meet your favorite artists, sport teams and parties</p>
+          <div class="hero-search">
+            <input type="text" placeholder="Seach Artist, Team, or Venue">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 @endsection
