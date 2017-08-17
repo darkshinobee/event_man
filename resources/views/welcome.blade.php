e318193f69348e091e79d2ca166cb3b5ae99e5da
darkshinobee <ahmedibrahim3000@gmail.com>
2017-08-17 12:13:30 +0100

    categories done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,38 +25,7 @@
         </div>
         <div class="section-content">
           <ul class="clearfix">
-            <li>
-              <div class="date">
-                <a href="#">
-                  <span class="day">25</span>
-                  <span class="month">August</span>
-                  <span class="year">2016</span>
-                </a>
-              </div>
-              <a href="#">
-                <img src="/images/zko.jpg" alt="image">
-              </a>
-              <div class="info">
-                <p>BMW Open Championship <span>San Francisco</span></p>
-                <a href="#" class="get-ticket">Get Ticket</a>
-              </div>
-            </li>
-            <li>
-              <div class="date">
-                <a href="#">
-                  <span class="day">26</span>
-                  <span class="month">August</span>
-                  <span class="year">2016</span>
-                </a>
-              </div>
-              <a href="#">
-                <img src="/images/zko.jpg" alt="image">
-              </a>
-              <div class="info">
-                <p>Kiai Kanjeng Orchestra <span>San Francisco</span></p>
-                <a href="#" class="get-ticket">Get Ticket</a>
-              </div>
-            </li>
+            @for ($i = 0; $i < 6; $i++)
             <li>
               <div class="date">
                 <a href="#">
@@ -73,10 +42,48 @@
                 <a href="#" class="get-ticket">Get Ticket</a>
               </div>
             </li>
+            @endfor
           </ul>
         </div>
       </div>
     </div>
   </section>
 
+  <section class="section-event-category">
+    <div class="container">
+      <div class="row">
+        <div class="section-header">
+          <h2>Event by Categories</h2>
+        </div>
+        <div class="section-content">
+          <ul class="row clearfix">
+            <li class="category-1 col-sm-4">
+              <img src="/theme/publish/images/event-category-1.jpg" alt="image">
+              <a href="#"><span>Concerts</span></a>
+            </li>
+            <li class="category-2 col-sm-4">
+              <img src="/theme/publish/images/event-category-2.jpg" alt="image">
+              <a href="#"><span>Sports</span></a>
+            </li>
+            <li class="category-3 col-sm-4">
+              <img src="/theme/publish/images/event-category-3.jpg" alt="image">
+              <a href="#"><span>Threaters</span></a>
+            </li>
+            <li class="category-4 col-sm-4">
+              <img src="/theme/publish/images/event-category-4.jpg" alt="image">
+              <a href="#"><span>Parties</span></a>
+            </li>
+            <li class="category-5 col-sm-4">
+              <img src="/theme/publish/images/event-category-5.jpg" alt="image">
+              <a href="#"><span>Communities</span></a>
+            </li>
+            <li class="category-6 col-sm-4">
+              <img src="/theme/publish/images/event-category-6.jpg" alt="image">
+              <a href="#"><span>Classes</span></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
 @endsection
