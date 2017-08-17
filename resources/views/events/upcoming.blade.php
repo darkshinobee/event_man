26e6dbb53f58705eda2768eee1a71ae07631a015
darkshinobee <ahmedibrahim3000@gmail.com>
2017-08-17 12:01:15 +0100

    cart removed, upcoming event fix

--- resources/views/events/upcoming.blade.php
+++ resources/views/events/upcoming.blade.php
@@ -18,34 +18,58 @@
     </div>
   </section>
 
-  <section class="section-calendar-events">
+  <section class="section-search-content">
     <div class="container">
       <div class="row">
-        <div class="section-content">
-          <div class="tab-content">
-              <div role="tabpanel" class="tab-pane active" id="tab1">
-              <ul class="clearfix">
-                @for ($i = 0; $i < 6; $i++)
-                <li>
-                  <div class="date">
-                    <a href="#">
-                      <span class="day">29</span>
-                      <span class="month">May</span>
-                      <span class="year">2016</span>
-                    </a>
-                  </div>
-                  <a href="#">
-                    <img src="/images/zko.jpg" alt="image">
-                  </a>
-                  <div class="info">
-                    <p>UEFA Champions League <span>San Francisco</span></p>
-                    <a href="#" class="get-ticket">Get Ticket</a>
-                  </div>
-                </li>
-                @endfor
-              </ul>
+
+
+        <div id="primary" class="col-md-10 col-md-offset-1">
+
+          <div class="search-result-header">
+            <div class="row">
+              <div class="col-sm-7">
+                <h2>All Upcoming Sports Events</h2>
+                <span>Showing 1-10 of 32 Results</span>
+              </div>
+              <div class="col-sm-5">
+                <label>Sort By</label>
+                <select class="selectpicker dropdown">
+                  <option>Price: Low-High</option>
+                  <option>Price: High-Low</option>
+                </select>
+              </div>
             </div>
           </div>
+
+          @for ($i = 0; $i < 5; $i++)
+          <div class="search-result-item">
+            <div class="row">
+              <div class="search-result-item-info col-sm-9">
+                <h3>UCI Road World Championships</h3>
+                <ul class="row">
+                  <li class="col-sm-5 col-lg-6">
+                    <span>Venue</span>
+                    Rose Bowl (Pasadena, CA)
+                  </li>
+                  <li class="col-sm-4 col-lg-3">
+                    <span>Saturday</span>
+                    August. 20th, 2016
+                  </li>
+                  <li class="col-sm-3">
+                    <span>Time</span>
+                    07:00 PM
+                  </li>
+                </ul>
+              </div>
+              <div class="search-result-item-price col-sm-3">
+                <span>Price</span>
+                <strong>N200</strong>
+                <a href="#">Get Ticket</a>
+              </div>
+            </div>
+          </div>
+          @endfor
+
         </div>
       </div>
     </div>
