7543df524459dc7f92c26e2d114a0d040729eade
Ibrahim Ahmed <ahmedibrahim3000@gmail.com>
2017-10-29 11:17:43 +0100

    same safari fix

--- resources/views/partials/_requiredFix.blade.php
+++ resources/views/partials/_requiredFix.blade.php
@@ -1,38 +1,20 @@
 <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 
-{{-- <script type="text/javascript">
-$("form").submit(function(e) {
-
-var ref = $(this).find("[required]");
-
-$(ref).each(function(){
-if ( $(this).val() == '' )
-{
-alert("Required field should not be blank.");
-
-$(this).focus();
-
-e.preventDefault();
-return false;
-}
-});  return true;
-});
-</script> --}}
 <script type="text/javascript">
-// SAFARI REQUIRED CHECK
-$(document).ready(function () {
-"use strict";
 $("form").submit(function(e) {
-  var ref = $(this).find("[required]");
-  $(ref).each(function(){
-    if ( $(this).val() === '' )
-    {
-      alert("Required field should not be blank.");
-      $(this).focus();
-      e.preventDefault();
-      return false;
-    }
-  });  return true;
-});
+
+    var ref = $(this).find("[required]");
+
+    $(ref).each(function(){
+        if ( $(this).val() == '' )
+        {
+            alert("Required field should not be blank.");
+
+            $(this).focus();
+
+            e.preventDefault();
+            return false;
+        }
+    });  return true;
 });
 </script>
